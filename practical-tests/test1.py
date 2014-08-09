f9a1d4cd01a74d8e46beb70307d21023f2d8f9d0
Jamie Harvey <jamjamnorman@gmail.com>
2014-08-10 11:28:51 +1200

    Added Problem Set 2

--- practical-tests/test1.py
+++ practical-tests/test1.py
@@ -4,18 +4,16 @@
 # Problem Set 1
 
 # Q1
-
 # variable list was renamed to list_of_strings to avoid confict with builtins
 def join_list(list_of_strings):
     return "".join(list_of_strings)
 
-list_of_strings = ["learn ", "computer ", "science,", "at ", "Massey "]
+list_of_strings = ["learn ", "computer ", "science", "at ", "Massey "]
 print join_list(list_of_strings)
 
 # Q2
-
 def get_user_choice():
-    while true:
+    while True:
         command = raw_input("Command: ")
         valid_commands = ["f", "m", "s", "d", "q"]
         if command in valid_commands:
@@ -29,4 +27,22 @@ def get_user_choice():
 
 user_command = get_user_choice()
 print "You entered:", user_command
+
+# Problem Set 2
+
+# Q1
+for _ in xrange(10):
+    for i in xrange(10):
+        print i,
+    print ""
+
+print ""
+
+# Q2
+for i in xrange(10):
+    for _ in xrange(i):
+            print " ",
+    for j in xrange(10-i):
+        print j,
+    print ""
 
